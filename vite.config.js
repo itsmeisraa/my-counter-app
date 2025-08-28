85570711e2b8e25f6880491e04c4b722f27780eb
israa chiheb <israaisraa202000@gmail.com>
2025-08-28 18:35:51 +0100

    again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-counter-app>/', // <-- this is the important part
-});
-
+=======
+  base: '/my-counter-app',
+})
